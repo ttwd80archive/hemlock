46b484622e8f47554e07ec5289808aef9cd08da9
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-11-03 09:22:50 +0800

    fixed bad bcrypt rounds. 25 is crazy.

--- src/test/java/com/twistlet/hemlock/security/service/BCryptPasswordEncoderTests.java
+++ src/test/java/com/twistlet/hemlock/security/service/BCryptPasswordEncoderTests.java
@@ -1,5 +1,7 @@
 package com.twistlet.hemlock.security.service;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -8,7 +10,7 @@ public class BCryptPasswordEncoderTests {
 	@Test
 	public void testPassword() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
-		System.out.println(encoder.encode("diamond*64"));
+		assertTrue(encoder.matches("diamond*64", "$2a$10$DCD3My32T23h0lLSXHRzA.RG4/TrIEUfBHKF6hBQfUmXJaiZxDQby"));
 	}
 
 }

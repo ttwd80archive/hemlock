eadbdf928822605d121770d1574366a27c0a7d4c
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-11-03 13:46:38 +0800

    bcrypt password for user

--- src/test/java/com/twistlet/hemlock/security/service/BCryptPasswordEncoderTests.java
+++ src/test/java/com/twistlet/hemlock/security/service/BCryptPasswordEncoderTests.java
@@ -8,9 +8,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BCryptPasswordEncoderTests {
 
 	@Test
-	public void testPassword() {
+	public void testPasswordAdmin() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 		assertTrue(encoder.matches("diamond*64", "$2a$10$DCD3My32T23h0lLSXHRzA.RG4/TrIEUfBHKF6hBQfUmXJaiZxDQby"));
+	}
+
+	@Test
+	public void testPasswordUser01() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+		System.out.println(encoder.encode("ph3a9iab"));
+		assertTrue(encoder.matches("ph3a9iab", "$2a$10$cLDgv.Sis/mmHQz1.fv6cOVAkmqe3i14J5bP7O8OCJhjCOKKCmtNG"));
 	}
 
 }
